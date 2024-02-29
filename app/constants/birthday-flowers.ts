695e5bd45fb44c474e3a6de9fbd5369e2e6c7ca5
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2024-02-29 15:02:16 +0700

    Update birthday flowers

--- app/constants/birthday-flowers.ts
+++ app/constants/birthday-flowers.ts
@@ -1,26 +1,26 @@
 export const BIRTHDAY_FLOWERS = [
-    {
-        id: 'fe72c09c-f366-57df-bc76-1fc9a0c0fdfe',
-        name: "Little Tana",
-        price: 370000,
-        image: "/images/little-tana.webp",
-    },
-    {
-        id: '6bb386bb-de1e-5cdc-b79d-b4a5a68f3dcd',
-        name: "Little Tana",
-        price: 370000,
-        image: "/images/little-tana.webp",
-    },
-    {
-        id: '1c718d9b-3d7f-5811-95ee-c8df646c47a5',
-        name: "Little Tana",
-        price: 370000,
-        image: "/images/little-tana.webp",
-    },
-    {
-        id: '6ccdd470-1530-5d3f-9bca-a03b9bb26219',
-        name: "Little Tana",
-        price: 370000,
-        image: "/images/little-tana.webp",
-    }
-]
+  {
+    id: "fe72c09c-f366-57df-bc76-1fc9a0c0fdfe",
+    name: "Flower Cake",
+    price: 370000,
+    image: "/birthday-flowers/birthday-flowers.jpg"
+  },
+  {
+    id: "6bb386bb-de1e-5cdc-b79d-b4a5a68f3dcd",
+    name: "Flower Cake",
+    price: 370000,
+    image: "/birthday-flowers/birthday-flowers.jpg"
+  },
+  {
+    id: "1c718d9b-3d7f-5811-95ee-c8df646c47a5",
+    name: "Flower Cake",
+    price: 370000,
+    image: "/birthday-flowers/birthday-flowers.jpg"
+  },
+  {
+    id: "6ccdd470-1530-5d3f-9bca-a03b9bb26219",
+    name: "Flower Cake",
+    price: 370000,
+    image: "/birthday-flowers/birthday-flowers.jpg"
+  }
+];
